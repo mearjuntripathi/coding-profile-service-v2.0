6beed19fcb657e0547fe4d438c1765a8ef5451e2
mearjuntripathi <mearjuntripathi@gmail.com>
2026-05-23 12:43:45 +0530

    make change in gfg section to scrap a data fast

--- internal/scraper/gfg.go
+++ internal/scraper/gfg.go
@@ -1,36 +1,3 @@
-// package scraper
-
-// import (
-// 	"coding-profile-service/pkg/model"
-// 	"fmt"
-// )
-
-// // FetchGFG wraps FetchGFGHTML and returns StatsResponse
-// func FetchGFG(username string) (model.StatsResponse, error) {
-// 	stats, err := FetchGFGHTML(username)
-// 	if err != nil {
-// 		return model.StatsResponse{
-// 			Platform: "gfg",
-// 			Username: username,
-// 			Error:    fmt.Sprintf("could not fetch GFG data for user: %s (%v)", username, err),
-// 		}, err
-// 	}
-
-// 	return model.StatsResponse{
-// 		Platform:             "gfg",
-// 		Username:             username,
-// 		TotalSolved:          stats.TotalSolved,
-// 		EasySolved:           stats.EasySolved,
-// 		MediumSolved:         stats.MediumSolved,
-// 		HardSolved:           stats.HardSolved,
-// 		Streak:               stats.Streak,
-// 		MaxRating:            stats.MaxRating,
-// 		ContestsParticipated: stats.ContestsParticipated,
-// 		GlobalRank:           stats.GlobalRank,
-// 		CountryRank:          stats.CountryRank,
-// 	}, nil
-// }
-
 package scraper
 
 import (
